234fffd8bc85f163971d4c8cfc0e40a083c06806
GustavoHerreroNunes <ghnunes2010@gmail.com>
2022-08-18 10:41:35 -0300

    Add: resize function to resize canvas element

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 window.addEventListener('load', () => {
-    console.log("Iniciando jogo...");
+    game.initialize();
 });
+
+const game = {
+    area: {
+        initialize: () => {
+            game.area.canvas = document.querySelector("#game-area");
+            game.area.context = game.area.canvas.getContext("2d");
+            game.area.resize();
+        },
+
+        resize: () => {
+            game.area.canvas.width = window.innerWidth;
+            game.area.canvas.height = window.innerHeight;
+        }
+    },
+
+    initialize: () => {
+        console.log("Iniciando jogo...");
+        game.area.initialize();
+    },
+}
